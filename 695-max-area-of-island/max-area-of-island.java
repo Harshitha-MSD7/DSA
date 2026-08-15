ad45d15ff55f07b69cc320f7e6e6aec4b9438858
Harshitha Sathees Kumar <HARSHITHA.SATHEESKUMAR@GMAIL.COM>
2026-08-15 09:17:27 -0400

    Time: 3 ms (24.15%) | Memory: 46.4 MB (83.36%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.java
@@ -0,0 +1,44 @@
+class Solution {
+    private static final int[][] directions = {{1, 0}, {-1, 0},
+                                               {0, 1}, {0, -1}};
+    public int maxAreaOfIsland(int[][] grid) {
+        int ROWS = grid.length, COLS = grid[0].length;
+        int max_area = 0;
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (grid[r][c] == 1) {
+                    max_area = Math.max(bfs(grid, r, c), max_area);
+                    
+                }
+            }
+        }
+
+        return max_area;
+    }
+
+    private int bfs(int[][] grid, int r, int c) {
+        Queue<int[]> q = new LinkedList<>();
+        int area = 0;
+        grid[r][c] = 0;
+        q.add(new int[]{r, c});
+        area++;
+
+        while (!q.isEmpty()) {
+            int[] node = q.poll();
+            int row = node[0], col = node[1];
+
+            for (int[] dir : directions) {
+                int nr = row + dir[0], nc = col + dir[1];
+                if (nr >= 0 && nc >= 0 && nr < grid.length &&
+                    nc < grid[0].length && grid[nr][nc] == 1) {
+                    q.add(new int[]{nr, nc});
+                    grid[nr][nc] = 0;
+                    area++;
+                }
+            }
+        }
+
+        return area;
+    }
+}
