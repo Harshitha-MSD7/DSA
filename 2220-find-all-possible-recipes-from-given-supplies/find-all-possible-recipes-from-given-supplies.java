7f5ff6f180c84eaaa09f4717d86d17b11b11a9ff
H <HARSHITHA.SATHEESKUMAR@GMAIL.COM>
2026-08-29 00:43:57 -0400

    Time: 63 ms (54.73%) | Memory: 49 MB (58.33%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
@@ -1,52 +1,46 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        
-        List<String> ans = new ArrayList<>();
+        // Building the inDegree map
+        HashMap<String, Integer> inDegree = new HashMap<>();
+        // recipie -> # of dependent nodes
+        for(int i = 0; i<recipes.length; i++){
+            inDegree.put(recipes[i], ingredients.get(i).size());
+        }
 
+        // Bulding the adjacency list
+        // ingredients -> recipies
+        HashMap<String, List<String>> adj = new HashMap<>();
+        for(int j = 0; j<ingredients.size(); j++){
+            for(String s : ingredients.get(j)){
+                if(!adj.containsKey(s)){
+                    adj.put(s,new ArrayList<>());
+                }   
+                adj.get(s).add(recipes[j]);
+                
+            }
+        }
+
+        // No dependecies for the particualar nodes
+        List<String> res = new ArrayList<>();
         Queue<String> q = new LinkedList<>();
         for(String s : supplies){
             q.offer(s);
         }
 
-        // inDegree
-        HashMap<String, Integer> inDegree = new HashMap<>();
-        for(int i = 0; i< recipes.length; i++){
-            inDegree.put(recipes[i],ingredients.get(i).size());
-        }
-        // ingredients -> {recipies}
-        HashMap<String, List<String>> adj = new HashMap<>();
-
-        // adj list
-
-        for(int j = 0; j<ingredients.size(); j++){
-            for(String ingredient : ingredients.get(j)){
-                if(adj.containsKey(ingredient)){
-                    adj.get(ingredient).add(recipes[j]);
-                }
-                else{
-                    List<String> newList = new ArrayList<>();
-    newList.add(recipes[j]);
-    adj.put(ingredient, newList);
-                }
-            }
-        }
+        // Kahn's traversal
         while(!q.isEmpty()){
             String node = q.poll();
-
             if(adj.containsKey(node)){
-                for(String s : adj.get(node)){
-                    inDegree.put(s, inDegree.get(s) - 1);
-                    if(inDegree.get(s) == 0){
-                        ans.add(s);
-                        q.offer(s);
+                for(String it : adj.get(node)){
+                    inDegree.put(it,inDegree.get(it) - 1);
+                    if(inDegree.get(it) == 0){
+                        res.add(it);
+                        q.offer(it);
                     }
-
                 }
             }
 
-        }
-
-        return ans;
-
+        }   
+        return res;
     }
 }
