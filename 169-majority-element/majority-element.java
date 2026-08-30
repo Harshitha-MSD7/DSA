ebe6963e3635526a4596b7f4a15ce794edc99a1c
H <HARSHITHA.SATHEESKUMAR@GMAIL.COM>
2026-08-30 13:15:40 -0400

    Time: 1 ms (99.89%) | Memory: 54.9 MB (71.53%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        /*
+        3-> 2
+        2 -> 1
+
+        O(nlogn) 
+        O(1)       
+        
+        1 -> 3 
+        [1,1,1,2,2,2,2]
+        Arrays.sort(nums);
+        int count = 0;
+        Math.max(map.values(), max);
+
+        count # of times last seen appears
+        */
+        int last_seen = nums[0];
+        int count = 1;
+        int size = nums.length/2;
+        for(int i = 1; i<nums.length; i++){
+            if(last_seen != nums[i]){
+                if(count > 0) count--;
+                else{
+                    last_seen = nums[i];
+                    count++;
+                    if(count > size) return last_seen;
+                }
+            }
+            else {
+                count++;
+                if(count > size) return last_seen;
+            }
+        }
+
+
+        return last_seen;
+        
+    }
+}
