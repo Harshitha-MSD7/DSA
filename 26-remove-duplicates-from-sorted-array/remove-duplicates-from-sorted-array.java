ed8a88b601a06a06f6de7bd46229570f4cb91e16
H <HARSHITHA.SATHEESKUMAR@GMAIL.COM>
2026-09-01 11:01:03 -0400

    Time: 0 ms (100.00%) | Memory: 46.8 MB (43.41%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,5 +1,8 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        /*
+        Brute Force:
+
         Set<Integer> set = new TreeSet<>();
         for(int num : nums){
             set.add(num);
@@ -13,6 +16,24 @@ class Solution {
         }
 
         return size;
+        */
 
+        /*
+
+        Optimal solution
+
+        nums = [0,0,1,1,1,2,2,3,3,4]
+
+
+        */
+        int last_seen = 0;
+
+        for(int i = 1; i<nums.length; i++){
+            if(nums[last_seen] != nums[i]){
+                last_seen++;
+                nums[last_seen] = nums[i];
+            }
+        }
+        return last_seen+1; 
     }
 }
