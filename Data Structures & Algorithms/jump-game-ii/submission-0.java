3d2d935436a979e0f2f1258407b45a7ce440e11f
H <HARSHITHA.SATHEESKUMAR@GMAIL.COM>
2026-09-07 18:22:58 -0400

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    // [3,4, 1, 3, _, _, _ ]
+    public int jump(int[] nums) {
+        int i = 0;        
+        int jump = 0;
+        while(i<nums.length-1){
+            int max = 0;
+            int best = 0;
+            for(int j = i+1; j <= i+nums[i]; j++){
+                if(j+nums[j] > max){
+                    max = Math.max(max, j+nums[j]);
+                    best = j;
+                }
+            }
+            jump++;
+            i = i + best;
+        }
+
+        return jump;
+    }
+}
